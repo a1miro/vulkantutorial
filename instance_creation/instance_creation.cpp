f01a60c06e5926ef57c9018fb89eb5205a8cbc85
Andrei Mironenko <amironenko@ammbp.local>
2023-05-19 23:51:33 +0100

    Adding build configuration for MacOS

--- instance_creation/instance_creation.cpp
+++ instance_creation/instance_creation.cpp
@@ -4,9 +4,11 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <string>
 
-const uint32_t WIDTH = 800;
-const uint32_t HEIGHT = 600;
+static const uint32_t WIDTH = 800;
+static const uint32_t HEIGHT = 600;
+static std::string SYSTEM_NAME = HOST_SYSTEM_NAME;
 
 class HelloTriangleApplication {
 public:
@@ -70,6 +72,20 @@ private:
         createInfo.ppEnabledExtensionNames = glfwExtensions;
 
         createInfo.enabledLayerCount = 0;
+
+        std::vector<const char *> requiredExtensions;
+        for (uint32_t i = 0; i < glfwExtensionCount; i++)
+        {
+            requiredExtensions.emplace_back(glfwExtensions[i]);
+        }
+
+        if (SYSTEM_NAME == "Darwin")
+        {
+            requiredExtensions.emplace_back(VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME);
+            createInfo.flags |= VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+            createInfo.enabledExtensionCount = (uint32_t)requiredExtensions.size();
+            createInfo.ppEnabledExtensionNames = requiredExtensions.data();
+        }
 
         if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
             throw std::runtime_error("failed to create instance!");
